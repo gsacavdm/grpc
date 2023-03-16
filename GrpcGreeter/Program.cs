f28ea781c99d23a34c8ee78b7672e355293f93b2
Gerardo Saca <gsacavdm@live.com>
2023-03-15 23:45:40 -0700

    Add extra config for MacOs dev machine

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -1,9 +1,18 @@
 using GrpcGreeter.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+// See https://go.microsoft.com/fwlink/?linkid=2099682
+if (OperatingSystemUtils.IsMacOS())
+{
+  builder.WebHost.ConfigureKestrel(options => 
+  {
+    // Setup a HTTP/2 endpoint without TLS
+    options.ListenLocalhost(5293, o => o.Protocols = HttpProtocols.Http2);
+  });
+}
 
 // Add services to the container.
 builder.Services.AddGrpc();
